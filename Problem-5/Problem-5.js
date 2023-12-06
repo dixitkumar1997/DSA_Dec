5b80b4922bf920130f2ef05cd2f31215013cf78d
sarcasmic guy <dxtt22@gmail.com>
2023-12-06 22:13:24 +0530

    fifth day array problem

---
+++ Problem-5/Problem-5.js
@@ -0,0 +1,23 @@
+// Ques:- first letter to be capitalized.
+
+// Soln:-
+
+let input = "in_progress"
+
+let lower="abcdefghijklmnopqrstuvwxyz";
+let upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let arr=s.split("_")
+let new_arr=[]
+for(let i=0;i<arr.length;i++){
+let bag="";
+  for(let j=1;j<arr[i].length;j++){
+    for(let k=0;k<26;k++){
+      if(bag==="" && arr[i][0]===lower[k]){
+        bag+=upper[k]
+      }
+    }
+    bag+=arr[i][j]
+  }
+  new_arr.push(bag)
+}
+console.log(new_arr.join(" "))
