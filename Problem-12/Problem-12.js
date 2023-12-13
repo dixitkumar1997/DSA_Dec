3e12c73c9a0ff1c55bc9c720e38c01e1640f46de
sarcasmic guy <dxtt22@gmail.com>
2023-12-13 23:50:11 +0530

    twelveth day object problem

---
+++ Problem-12/Problem-12.js
@@ -0,0 +1,32 @@
+// Ques:-You are given an array of integers nums. You are also given an integer original which is the first number that needs to be searched for in nums.
+
+// You then do the following steps:
+
+// If original is found in nums, multiply it by two (i.e., set original = 2 * original).
+// Otherwise, stop the process.
+// Repeat this process with the new number as long as you keep finding the number.
+// Return the final value of original
+
+// Soln:-
+
+let nums = [5,3,6,1,12];
+let original = 3;
+
+
+function findFinalValue(nums, original) {
+    let num=original;
+   let i=0;
+   while(i<nums.length){
+       if(nums[i]==num){
+           num=num*2;
+           i=0;
+       }
+     else{
+       i++;
+     }
+   }
+   return num;
+};
+
+
+console.log(findFinalValue(nums, original))
